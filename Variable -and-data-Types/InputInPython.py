2925e2709bab5247ae775d0e1b40382af488dab1
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2025-01-09 21:25:36 +0530

    Variables and Data Types

---
+++ Variable -and-data-Types/InputInPython.py
@@ -0,0 +1,7 @@
+# input() statement is used to accept values (using keyboard) from user
+
+name = input("name:  ")
+age = int(input("age:  "))
+price = float(input("price:  "))
+
+print("My name is", name, "and I'm", age, "year's old and my wallet price is: ", price)
---
+++ Variable -and-data-Types/conditionalStatements.py
@@ -0,0 +1,12 @@
+name = input("Enter Your Name: ")
+age = int(input("Enter Your Age: "))
+
+if(age<18) :
+    print("Just Some years to gave a Vote!! because your age is", age)
+
+elif(age == 18) :
+    print("Great Your are on the way because your have just on", age)
+
+else:
+    print("just Go brother...")
+    
